a0ddc792723897e553a01377737883cda73c8432
hendrysiak <lukasz.hendrysiak@gmail.com>
2020-02-11 21:17:08 +0100

    Added loader

---
+++ client/src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="ldsRoller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -3,26 +3,33 @@ import "./App.css";
 import EditNote from "../../components/EditNote/EditNote";
 import Notes from "../../components/Notes/Notes";
 import axios from "axios";
+import Loader from "../../components/UI/Loader/Loader";
 
 const App = () => {
   const [data, setData] = useState<[]>([]);
   const [editNote, setEditNote] = useState<string>("");
   const [shouldUpdate, setUpdateStatus] = useState<boolean>(false);
-  const [isLoading, setLoadingState] = useState<boolean>(false);
+  const [isLoading, setLoadingState] = useState<boolean>(true);
 
   useEffect(() => {
+    // setLoadingState(true);
     const getData = async () => {
       try {
         const result = await axios.get("http://localhost:4000/notes");
         setData(result.data);
+        setLoadingState(false);
       } catch (err) {
         console.log(err);
+        setLoadingState(false);
       }
     };
     getData();
   }, [shouldUpdate]);
 
-  useEffect(() => setUpdateStatus(false), [shouldUpdate]);
+  useEffect(() => {
+    setUpdateStatus(false);
+    setLoadingState(false);
+  }, [shouldUpdate]);
 
   const changeEvent = (event: any) => {
     event.stopPropagation();
@@ -31,6 +38,7 @@ const App = () => {
   };
 
   const updateNotes = () => {
+    setLoadingState(false);
     setUpdateStatus(true);
   };
 
@@ -52,7 +60,8 @@ const App = () => {
             : null}
         </ul>
 
-        {<EditNote isEdit={editNote} shouldUpdate={() => updateNotes()} />}
+        <EditNote isEdit={editNote} shouldUpdate={() => updateNotes()} />
+        {isLoading ? <Loader /> : ""}
       </div>
     </div>
   );
